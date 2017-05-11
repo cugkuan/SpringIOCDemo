997fc581ccb26d1a15a902bb3ef1c4321264fdbe
kuan <kuan177>
2017-05-11 18:22:31 +0800

    first

---
+++ src/main/java/springexample/creditlinking/CreditLinking.java
@@ -0,0 +1,29 @@
+package springexample.creditlinking;
+
+import springexample.domain.ICustomer;
+
+/**
+ * 信用连接
+ * @author Kuan
+ *
+ */
+public class CreditLinking implements CreditLinkingInterface {
+
+	private String url;
+
+	public void linkCreditBankAccount(ICustomer icustomer) throws Exception {
+		// Connect to URL
+		System.out.println("url to connect is" + url);
+
+		System.out.println("credit card linked for customer id " + icustomer.getCustomerId());
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
---
+++ src/main/java/springexample/creditrating/CreditRating.java
@@ -0,0 +1,16 @@
+package springexample.creditrating;
+
+import springexample.domain.ICustomer;
+
+/**
+ * 信用评级
+ * @author Kuan
+ *
+ */
+public class CreditRating implements CreditRatingInterface {
+
+	  public boolean getUserCreditHistoryInformation(ICustomer iCustomer){
+		  
+		  return true;
+	  }
+}
